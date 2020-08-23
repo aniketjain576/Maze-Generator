895b749ce5e96d93f0b8564d74639658c27a6635
aniket <aniketjain576@gmail.com>
2020-08-22 19:55:21 -0700

    added action to remove walls

--- src/1-components/Cell/ducks/actions.ts
+++ src/1-components/Cell/ducks/actions.ts
@@ -1,21 +1,30 @@
 import { UPDATED_CELL } from './types';
-import { Cell, NormalThunk } from '../../interfaces';
+import { Cell, NormalThunk, Walls } from '../../interfaces';
 
 export function doMarkVisited(index: number): NormalThunk {
   return (dispatch, getState) => {
     const cells: Cell[] = getState().grid.cells;
     const visitedCell: Cell = JSON.parse(JSON.stringify(cells[index]));
     visitedCell.visited = true;
-    dispatch(doUpdateCell(visitedCell, index));
+    dispatch(doUpdateCell(index, visitedCell));
   };
 }
 
-function doUpdateCell(cell: Cell, index: number) {
+export function doUpdateWalls(index: number, walls: Walls): NormalThunk {
+  return (dispatch, getState) => {
+    const cells: Cell[] = getState().grid.cells;
+    const targetCell: Cell = JSON.parse(JSON.stringify(cells[index]));
+    targetCell.walls = walls;
+    dispatch(doUpdateCell(index, targetCell));
+  };
+}
+
+function doUpdateCell(index: number, cell: Cell) {
   return {
     type: UPDATED_CELL,
     payload: {
-      cell: cell,
       index: index,
+      cell: cell,
     },
   };
 }
