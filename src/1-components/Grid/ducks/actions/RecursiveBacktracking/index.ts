3e29c6d66ce40eea4a27d09dbddb7ff65145b03e
aniket <aniketjain576@gmail.com>
2020-08-23 09:00:36 -0700

    added framework to remove walls

--- src/1-components/Grid/ducks/actions/RecursiveBacktracking/index.ts
+++ src/1-components/Grid/ducks/actions/RecursiveBacktracking/index.ts
@@ -1,8 +1,8 @@
 import { doMarkVisited } from '../../../../Cell/ducks/actions';
 import { NormalThunk, Cell } from '../../../../interfaces';
 import { doSetCurrentCell } from '../actions';
-import { CELLS_PER_ROW } from '../../../../constants';
 import { findNeighbors } from './helpers';
+import { CELLS_PER_ROW } from '../../../../constants';
 
 export function doRecursiveBacktracking(): NormalThunk {
   return (dispatch, getState) => {
@@ -10,15 +10,43 @@ export function doRecursiveBacktracking(): NormalThunk {
       const currentCellIndex = getState().currentCell.currentCellIndex;
       const cells: Cell[] = getState().grid.cells;
 
-      const next = findNeighbors(currentCellIndex, cells);
-      if (next) {
-        dispatch(doMarkVisited(next));
-        dispatch(doSetCurrentCell(next));
+      const nextCellIndex = findNeighbors(currentCellIndex, cells);
+      if (nextCellIndex) {
+        dispatch(doMarkVisited(nextCellIndex));
+        //remove wall between cuurentCellIndex and nextCellIndex
+        //removeWalls(currentCellIndex, nextCellIndex);
+        dispatch(doSetCurrentCell(nextCellIndex));
       } else {
         //backtracking will go here
         clearInterval();
       }
     }, 80);
+
+    function removeWalls(currentCell: number, nextCell: number) {
+      if (currentCell - nextCell === -1) {
+        //remove right wall of current
+        //remove left wall of next
+      }
+      if (currentCell - nextCell === 1) {
+        //remove left wall of current
+        //remove rigth wall of next
+      }
+      if (currentCell - nextCell === CELLS_PER_ROW) {
+        //remove top wall of current
+        //remove bottom wall of next
+      }
+      if (currentCell - nextCell === -1 * CELLS_PER_ROW) {
+        //remove bottom wall of current
+        //remove top wall of next
+      }
+    }
+
+    enum WALL_TYPES {
+      TOP = 'TOP',
+      RIGHT = 'RIGHT',
+      BOTTOM = 'BOTTOM',
+      LEFT = 'LEFT',
+    }
 
     // const currentCellIndex = getState().currentCell.currentCellIndex;
     // const cells: Cell[] = getState().grid.cells;
