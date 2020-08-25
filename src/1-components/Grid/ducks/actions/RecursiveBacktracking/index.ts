8d69abc7677c1bcffe3922d9e6b43da62f34a6ad
aniket <aniketjain576@gmail.com>
2020-08-25 08:06:58 -0700

    abstracted move to next and previous cell

--- src/1-components/Grid/ducks/actions/RecursiveBacktracking/index.ts
+++ src/1-components/Grid/ducks/actions/RecursiveBacktracking/index.ts
@@ -17,44 +17,60 @@ export function doRecursiveBacktracking(): NormalThunk {
       timer.start();
 
       const state = getState();
-
       const currentCellIndex = state.grid.currentCellIndex;
       const cells: Cell[] = state.grid.cells;
       const stack: Cell[] = state.grid.stack;
 
       const nextCellIndex = findNeighbors(currentCellIndex, cells);
       if (nextCellIndex) {
-        const newCells = JSON.parse(JSON.stringify(cells));
-        const currentCell = newCells[currentCellIndex];
-        const nextCell = newCells[nextCellIndex];
-        const newStack = [...stack, currentCell];
-
-        removeWallsBetweenCells(currentCell, nextCell);
-
-        markNextCellVisited(nextCell);
-
-        dispatch({
-          type: MOVED_TO_NEXT_CELL,
-          payload: {
-            cells: newCells,
-            stack: newStack,
-            currentCellIndex: nextCellIndex,
-          },
-        });
+        dispatch(moveToNextCell(nextCellIndex));
       } else if (stack.length > 0) {
-        const previousCell: Cell = stack.slice(-1)[0];
-        dispatch({
-          type: MOVED_TO_PREVIOUS_CELL,
-          payload: {
-            stack: stack.slice(0, stack.length - 1),
-            currentCellIndex: previousCell.index,
-          },
-        });
+        dispatch(moveToPreviousCell());
       } else {
         clearInterval(backtracking);
       }
       timer.stop();
     }, 0);
     dispatch(doSetInterval(backtracking));
+  };
+}
+
+function moveToNextCell(nextCellIndex: number): NormalThunk {
+  return (dispatch, getState) => {
+    const state = getState();
+    const currentCellIndex = state.grid.currentCellIndex;
+    const cells: Cell[] = state.grid.cells;
+    const stack: Cell[] = state.grid.stack;
+    const newCells = JSON.parse(JSON.stringify(cells));
+    const currentCell = newCells[currentCellIndex];
+    const nextCell = newCells[nextCellIndex];
+    const newStack = [...stack, currentCell];
+
+    removeWallsBetweenCells(currentCell, nextCell);
+
+    markNextCellVisited(nextCell);
+
+    dispatch({
+      type: MOVED_TO_NEXT_CELL,
+      payload: {
+        cells: newCells,
+        stack: newStack,
+        currentCellIndex: nextCellIndex,
+      },
+    });
+  };
+}
+
+function moveToPreviousCell(): NormalThunk {
+  return (dispatch, getState) => {
+    const stack = getState().grid.stack;
+    const previousCell: Cell = stack.slice(-1)[0];
+    dispatch({
+      type: MOVED_TO_PREVIOUS_CELL,
+      payload: {
+        stack: stack.slice(0, stack.length - 1),
+        currentCellIndex: previousCell.index,
+      },
+    });
   };
 }
--- src/1-components/constants.ts
+++ src/1-components/constants.ts
@@ -1,4 +1,4 @@
 export const GRID_SIZE = 400;
-export const CELL_SIZE = 40;
+export const CELL_SIZE = 20;
 export const TOTAL_NUM_CELLS = Math.pow(GRID_SIZE / CELL_SIZE, 2);
 export const CELLS_PER_ROW = GRID_SIZE / CELL_SIZE;
